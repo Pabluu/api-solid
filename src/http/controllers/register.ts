c79ebf7108d4c340d2be277ca2758783f31443dc
Pablo Henrique SC <pabloph0077@gmail.com>
2023-05-28 15:45:55 -0300

    ativando o eslint e removendo partes "incorretas"

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,8 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
-import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -23,8 +23,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (error) {
-    if (error instanceof UserAlreadyExistsError){
-      return reply.status(409).send({ message: error.message})
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
     }
 
     throw error
--- src/use-cases/errors/register.spec.ts
+++ src/use-cases/errors/register.spec.ts
@@ -1,7 +1,7 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { compare } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from '../register'
-import { compare } from 'bcryptjs'
-import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { UserAlreadyExistsError } from './user-already-exists-error'
 
 describe('Register Use Case', () => {
