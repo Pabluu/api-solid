b2344cc9e26cf632158207c147590001f86f0dc8
Pablo Henrique SC <pabloph0077@gmail.com>
2023-08-11 13:19:33 -0300

    feat: InMemory Databases

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,26 +1,29 @@
 import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+// import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    // const prismaUsersRepository = new PrismaUsersRepository()
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Pablo Henrique',
+      email: 'pablo.@gmail.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
   it('should hash user password upon registration', async () => {
     // const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail(email) {
-        return null
-      },
-
-      async create(data) {
-        return {
-          id: 'user-1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
-    })
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
 
     const { user } = await registerUseCase.execute({
       name: 'Pablo Henrique',
@@ -34,5 +37,27 @@ describe('Register Use Case', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    // const prismaUsersRepository = new PrismaUsersRepository()
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const email = 'pablo.ph@email.com'
+
+    await registerUseCase.execute({
+      name: 'Pablo Henrique',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'Pablo Henrique',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
